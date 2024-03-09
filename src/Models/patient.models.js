629e26105c24ab91d2af50979cdc69ed2ac261be
Harsh Arora <officialharsharora2812@gmail.com>
2024-03-09 16:54:44 +0530

    error fix

--- src/Models/patient.models.js
+++ src/Models/patient.models.js
@@ -39,18 +39,18 @@ const patientSchema = new Schema({
 
 },{timestamps : true})
 
-userSchema.pre("save" ,async function (next) {
+patientSchema.pre("save" ,async function (next) {
   if(!this.isModified("password")) return next()
 
     this.password = await bcrypt.hash(this.password,10)
    next()
 })
 
-userSchema.methods.isPasswordCorrect = async function (password){
+patientSchema.methods.isPasswordCorrect = async function (password){
   return await bcrypt.compare(password,this.password)
 }
 
-userSchema.methods.generateAccessToken =  function (){
+patientSchemaSchema.methods.generateAccessToken =  function (){
    return jwt.sign(
     {
       id : this._id,
@@ -63,7 +63,7 @@ userSchema.methods.generateAccessToken =  function (){
     })
 }
 
-userSchema.methods.generateRefreshToken =  function (){
+patientSchema.methods.generateRefreshToken =  function (){
   return jwt.sign(
     {
       id : this._id,
