403ded0b423e13a110d374d4f76ef7b0eb2adc17
Harsh Arora <officialharsharora2812@gmail.com>
2024-03-09 15:51:30 +0530

    initializing project

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+import {app} from "./app.js"
+
+dotenv.config({
+  path: './.env'
+})
+
+connectDB()
+.then(() => {
+  
+  app.listen(process.env.PORT || 8000 , () => {
+    console.log(`app listening on ${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+  console.log("MongoDB connection Failed : ",err);
+})
+
