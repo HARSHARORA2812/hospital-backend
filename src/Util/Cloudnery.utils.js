0aaf66c22fbe1f6fdd239b85d71c6a3a09fb38f3
Harsh Arora <officialharsharora2812@gmail.com>
2024-03-10 23:55:35 +0530

    cloudnery utility added

---
+++ src/Util/Cloudnery.utils.js
@@ -0,0 +1,46 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs" 
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECREAT 
+});
+
+
+const uploadOnCloudibary = async (localFilePath) => {
+  try {
+
+    if(!localFilePath) return null
+
+    const response = await cloudinary.uploader.upload(localFilePath,{
+      resource_type : "auto"
+    })
+
+    fs.unlinkSync(localFilePath);
+
+    return response;
+    
+  } catch (error) {
+      fs.unlinkSync(localFilePath);
+      return null
+  }
+}
+
+const deleteFromCloudinary = async (oldUrl) => {
+  try {
+
+    if(!oldUrl) return null
+
+    const response = await cloudinary.uploader.destroy(oldUrl,{
+      resource_type : "auto"
+    })
+
+    return response;
+}
+catch (error) {
+  return null
+}
+}
+
+export {uploadOnCloudibary  , deleteFromCloudinary}
